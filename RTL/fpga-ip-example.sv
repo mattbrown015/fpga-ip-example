0124d11e16e347c56038ea55aa6d5ac6047955cb
Matt Brown <matt_d_br@hotmail.com>
2020-06-22 09:41:30 +0100

    Added empty design, testbench and constraints files.
    I'm using the same directory structure as 'simple-fpga-cvs'.
    
    I got this directory structure from Vivado Design Suite User Guide Design Flows Overview UG892.
    
    I have added the oscillator inputs because we're going to need a clock. I copied the clock input names from the Clock Wizard IP example.
    
    The Clock Wizard doesn't specify the input the example, i.e. 'logic', so I haven't.
    
    And, we know we have to specify the configuration voltage for the KCU105 in the constraints.

---
+++ RTL/fpga-ip-example.sv
@@ -0,0 +1,7 @@
+`timescale 1ns / 1ps
+
+module fpga_ip_example(
+    input clk_in1_p,
+    input clk_in1_n
+    );
+endmodule
---
+++ Testbenches/fpga-ip-example-tb.sv
@@ -0,0 +1,4 @@
+`timescale 1ns / 1ps
+
+module fpga_ip_example_tb();
+endmodule
