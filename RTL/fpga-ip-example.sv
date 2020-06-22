95c9f2ae9de5b4b045a17ca254f6888042fed179
Matt Brown <matt_d_br@hotmail.com>
2020-06-22 21:34:57 +0100

    Added AXI GPIO IP core and joined it to the AXI traffic generator.
    The AXI traffic generator wasn't doing much on its own, it needs a slave to respond before any traffic actually flows.
    
    Adding the a AXI GPIO made it possible to seem the AXI traffic in the simulation waveform.
    
    I had to increase 'Max Clocks to Run', before I did this the data got stuck at '2'.
    
    AXI GPIO v2.0 LogiCORE IP Product Guide PG144 tells us that address 0 in the AXI GPIO is the GPIO data which is a R/W register. So by changing the address to 0 in address.coe we write various values to GPIO data! :-)

--- RTL/fpga-ip-example.sv
+++ RTL/fpga-ip-example.sv
@@ -2,23 +2,27 @@
 
 module fpga_ip_example(
     input clk_in1_p,
-    input clk_in1_n
+    input clk_in1_n,
+    output [7:0]gpio_io_o
     );
 
     logic s_axi_aresetn = 1;
     logic [31:0]m_axi_lite_ch1_awaddr;
     logic [2:0]m_axi_lite_ch1_awprot;
     logic m_axi_lite_ch1_awvalid;
-    logic m_axi_lite_ch1_awready = 0;
+    logic m_axi_lite_ch1_awready;
     logic [31:0]m_axi_lite_ch1_wdata;
     logic [3:0]m_axi_lite_ch1_wstrb;
     logic m_axi_lite_ch1_wvalid;
-    logic m_axi_lite_ch1_wready = 0;
-    logic [1:0]m_axi_lite_ch1_bresp = 0;
-    logic m_axi_lite_ch1_bvalid = 0;
+    logic m_axi_lite_ch1_wready;
+    logic [1:0]m_axi_lite_ch1_bresp;
+    logic m_axi_lite_ch1_bvalid;
     logic m_axi_lite_ch1_bready;
     logic done;
     logic [31:0]status;
+    logic [8:0]s_axi_araddr = 0;
+    logic s_axi_arvalid = 0;
+    logic s_axi_rready = 0;
 
     clk_wiz_0 clk_wiz_0
     (
@@ -46,5 +50,29 @@ module fpga_ip_example(
         .m_axi_lite_ch1_bready(m_axi_lite_ch1_bready),    // output wire m_axi_lite_ch1_bready
         .done(done),                                      // output wire done
         .status(status)                                   // output wire [31 : 0] status
+    );
+
+    axi_gpio_0 axi_gpio_0
+    (
+        .s_axi_aclk(clk_out1),                   // input wire s_axi_aclk
+        .s_axi_aresetn(s_axi_aresetn),           // input wire s_axi_aresetn
+        .s_axi_awaddr(m_axi_lite_ch1_awaddr),    // input wire [8 : 0] s_axi_awaddr
+        .s_axi_awvalid(m_axi_lite_ch1_awvalid),  // input wire s_axi_awvalid
+        .s_axi_awready(m_axi_lite_ch1_awready),  // output wire s_axi_awready
+        .s_axi_wdata(m_axi_lite_ch1_wdata),      // input wire [31 : 0] s_axi_wdata
+        .s_axi_wstrb(m_axi_lite_ch1_wstrb),      // input wire [3 : 0] s_axi_wstrb
+        .s_axi_wvalid(m_axi_lite_ch1_wvalid),    // input wire s_axi_wvalid
+        .s_axi_wready(m_axi_lite_ch1_wready),    // output wire s_axi_wready
+        .s_axi_bresp(m_axi_lite_ch1_bresp),      // output wire [1 : 0] s_axi_bresp
+        .s_axi_bvalid(m_axi_lite_ch1_bvalid),    // output wire s_axi_bvalid
+        .s_axi_bready(m_axi_lite_ch1_bready),    // input wire s_axi_bready
+        .s_axi_araddr(s_axi_araddr),             // input wire [8 : 0] s_axi_araddr
+        .s_axi_arvalid(s_axi_arvalid),           // input wire s_axi_arvalid
+        .s_axi_arready(s_axi_arready),           // output wire s_axi_arready
+        .s_axi_rdata(s_axi_rdata),               // output wire [31 : 0] s_axi_rdata
+        .s_axi_rresp(s_axi_rresp),               // output wire [1 : 0] s_axi_rresp
+        .s_axi_rvalid(s_axi_rvalid),             // output wire s_axi_rvalid
+        .s_axi_rready(s_axi_rready),             // input wire s_axi_rready
+        .gpio_io_o(gpio_io_o)                    // output wire [7 : 0] gpio_io_o
     );
 endmodule
--- Testbenches/fpga-ip-example-tb.sv
+++ Testbenches/fpga-ip-example-tb.sv
@@ -5,6 +5,7 @@ module fpga_ip_example_tb();
 
     logic osc_300_p;
     logic osc_300_n;
+    logic [7:0]gpio_io_o;
 
     always begin
         osc_300_p = 1'b0;
@@ -14,5 +15,5 @@ module fpga_ip_example_tb();
         #(osc_300_period / 2);
     end
 
-    fpga_ip_example fpga_ip_example(osc_300_p, osc_300_n);
+    fpga_ip_example fpga_ip_example(osc_300_p, osc_300_n, gpio_io_o);
 endmodule
