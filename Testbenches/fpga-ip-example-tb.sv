12f2ab0ce6433c9cc56dbdbb2211b878c178eb40
Matt Brown <matt_d_br@hotmail.com>
2020-06-22 09:59:11 +0100

    Added simulation clock source to testbench.
    There's a waveform in the simulation now. :-)
    
    I got this from the language templates but used my own, more descriptive, names.
    
    Some might say the names are too descriptive because if the oscillator frequency was changed all the names would need changing. In a small example project, for a newbie, it helps to be overly descriptive.

--- Testbenches/fpga-ip-example-tb.sv
+++ Testbenches/fpga-ip-example-tb.sv
@@ -1,4 +1,16 @@
 `timescale 1ns / 1ps
 
 module fpga_ip_example_tb();
+    localparam osc_300_period = 3.333;
+
+    logic osc_300_p;
+    logic osc_300_n;
+
+    always begin
+        osc_300_p = 1'b0;
+        osc_300_n = 1'b1;
+        #(osc_300_period / 2) osc_300_p = 1'b1;
+        osc_300_n = 1'b0;
+        #(osc_300_period / 2);
+    end
 endmodule
