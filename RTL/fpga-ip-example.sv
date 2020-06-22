cbc001b4d2f8cb51785a34ce533e812856dc47c8
Matt Brown <matt_d_br@hotmail.com>
2020-06-22 10:40:06 +0100

    Added PLL using IP Clocking Wizard.
    This is my first attempt at using IP from the IP Catalog.
    
    The clock is simple enough that it is not hard to use without the Clock Wizard but Clock Wizard is nice.
    
    Vivado Design Suite User Guide Design Flows Overview UG892 says it is only necessary to add the 'The IP source file (.xci)' to the repo, so that what I've done. I'm assuming all the other files can be generated using the 'Generate Output Products...' menu.

--- RTL/fpga-ip-example.sv
+++ RTL/fpga-ip-example.sv
@@ -4,4 +4,13 @@ module fpga_ip_example(
     input clk_in1_p,
     input clk_in1_n
     );
+
+    clk_wiz_0 clk_wiz_0
+    (
+        // Clock out ports
+        .clk_out1(clk_out1),      // output clk_out1
+        // Clock in ports
+        .clk_in1_p(clk_in1_p),    // input clk_in1_p
+        .clk_in1_n(clk_in1_n)     // input clk_in1_n
+    );
 endmodule
